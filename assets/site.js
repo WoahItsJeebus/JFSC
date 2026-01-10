75b498d122f51e54d8a56714ad17be981780e357
WoahItsJeebus <147510817+WoahItsJeebus@users.noreply.github.com>
2026-01-09 21:11:06 -0600

    Adjusted padding for edit button on since column

--- assets/site.js
+++ assets/site.js
@@ -10,7 +10,7 @@
 
 
 let __orbBgStop = null;
-export const JSTC_VERSION = "1.0.16";
+export const JSTC_VERSION = "1.0.17";
 
 export function startOrbBackground(opts = {}) {
 	const reduce = window.matchMedia?.("(prefers-reduced-motion: reduce)")?.matches;
